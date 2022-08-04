5ce356f2374027a49b527ea4789b4fe252d31038
Your Name <you@example.com>
2022-08-04 20:28:46 +0700

    add file

--- src/Components/PokemonList.js
+++ src/Components/PokemonList.js
@@ -1,58 +1,53 @@
 import axios from "axios";
-import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 
 const API_ENDPOINT = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=200";
-class PokemonList extends React.Component {
-    state = {
-        pokemons: [],
-        loading: false
-    };
-    componentDidMount() {
-        this.setState({loading: true});
+function PokemonList() {
+    const [pokemons, setPokemons] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
         axios
             .get(API_ENDPOINT)
             .then((res) => {
                 const pokemons = res.data.results;
-                this.setState({ pokemons: pokemons });
+                setPokemons(pokemons);
             })
             .catch((error) => console.log(error))
             .finally(() => {
-                this.setState({loading: false});
-            })
-    }
-    render() {
-        const {loading, pokemons} = this.state;
-        if (loading) return (
-            <div>
-                <p>Loading...</p>
-            </div>
-        )
-        return (
-            <table className="table table-bordered">
-                <thead>
+                setLoading(false);
+            });
+    });
+    return (
+        <table className="table table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Pokemon Name</th>
+                    <th scope="col">URL</th>
+                </tr>
+            </thead>
+            <tbody>
+                {pokemons.map((pokemon, index) => (
                     <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Pokemon Name</th>
-                        <th scope="col">URL</th>
+                        <th scope="row">{index}</th>
+                        <td>{pokemon.name}</td>
+                        <td>
+                            <a
+                                href={pokemon.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                {pokemon.url}
+                            </a>
+                        </td>
                     </tr>
-                </thead>
-                <tbody>
-                    {pokemons.map((pokemon, index) => (
-                        <tr>
-                            <th scope="row">{index}</th>
-                            <td>{pokemon.name}</td>
-                            <td>
-                                <a href={pokemon.url} target="_blank" rel="noopener noreferrer">
-                                    {pokemon.url}
-                                </a>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        );
-    }
+                ))}
+            </tbody>
+        </table>
+    );
 }
 
 export default PokemonList;
