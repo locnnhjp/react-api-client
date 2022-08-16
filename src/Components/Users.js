cd6249bf56b55802c9e2ac014dc7ea64c2fb7698
locnnh <t13cs013uy@gmail.com>
2022-08-16 09:10:22 +0700

    add file

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -3,33 +3,49 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 function Users() {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     let navigate = useNavigate();
     useEffect(() => {
-        axios.get("http://localhost:3001/api/users")
-        .then((res) => {
-            setUsers(res.data);
-        })
-        .catch(error => console.log(error));
-    });
+        getUsers()
+            .then((res) => setUsers(res.data))
+            .catch((err) => console.log(err))
+            .finally(() => {
+                setLoading(false);
+            });
+    }, [loading]);
+
+    const getUsers = () => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                axios
+                    .get("http://localhost:3001/api/users")
+                    .then((res) => resolve(res))
+                    .catch((err) => reject(err));
+            }, 1000);
+        });
+    };
 
     const handleCreate = () => {
         navigate("/user/add");
-    }
+    };
 
-    return(
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+    return (
         <div>
-            <h1>Use</h1>
-            {users.map(user => (
+            <h1>Users</h1>
+            {users.map((user) => (
                 <div key={user.id}>
                     <a href={`user/${user.id}`}>{user.name}</a>
                 </div>
             ))}
             <button onClick={handleCreate}>Create</button>
         </div>
-    )
+    );
 }
 
 export default Users;
